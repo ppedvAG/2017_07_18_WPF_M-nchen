1e0e68c91ba893aff40aabaeba8e549790a1cc51
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-07-18 16:36:35 +0200

    Resources

---
+++ Resources/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Resources.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StaticResourceChangeValue_Click(object sender, RoutedEventArgs e)
+        {
+            var colorBrush = Resources["defaultBackgroundColor"];
+
+            if (colorBrush is SolidColorBrush c)
+                c.Color = Colors.Blue;
+
+        }
+
+        private void StaticResourceChangeInstance_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["defaultBackgroundColor"] = new SolidColorBrush(Colors.Fuchsia);
+        }
+
+        private void OpenNewWindow(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+
+        private void StaticResourceChangeType_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["defaultBackgroundColor"] = new RadialGradientBrush(Colors.Fuchsia, Colors.Gold);
+        }
+    }
+}
---
+++ Resources/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace Resources
+{
+    public static class Settings
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return Properties.Resources.ConnectionString;
+            }
+        }
+    }
+}
