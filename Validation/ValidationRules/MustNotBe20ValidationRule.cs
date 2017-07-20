cac66a8bd7018ca52c8e876ba796da9cb6294d7f
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-07-20 15:07:25 +0200

    Validation

---
+++ Validation/ValidationRules/MustNotBe20ValidationRule.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation.ValidationRules
+{
+    public class MustNotBe20ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string s && int.TryParse(s, out int i) && i == 20)
+                return new ValidationResult(false, "Must not be 20!");
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ Validation/ValidationRules/MustNotBeLessThan50ValdiationRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation.ValidationRules
+{
+    public class MustNotBeLessThan50ValdiationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string s && int.TryParse(s, out int i) && i < 50)
+                return new ValidationResult(false, "Must not be less than 50!");
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
