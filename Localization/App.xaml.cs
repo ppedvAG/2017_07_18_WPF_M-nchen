415bb3410a8a2155d16ca94cc30b952aaf3daa61
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-07-20 16:03:37 +0200

    Localization

---
+++ Localization/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Localization
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-ES");
+        }
+    }
+}
