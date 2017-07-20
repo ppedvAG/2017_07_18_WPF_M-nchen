594302bb84d1bb081513b33319a9a8e77277f7d0
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-07-20 09:29:27 +0200

    Wiederholung Tag 2

--- ConvertersExample/Converters/BoolToBrushConverter.cs
+++ ConvertersExample/Converters/BoolToBrushConverter.cs
@@ -10,10 +10,7 @@ namespace ConvertersExample.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (parameter != null && parameter is string[] colors)
-            {
                 return (bool)value ? colors[0] : colors[1];
-            }
-
 
             return (bool)value ? Brushes.Green : Brushes.Red;
         }
